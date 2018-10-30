ceaee7459b53cb0148f6076d071c58cb7dc8030c
jerry221a <44384611+jerry221a@users.noreply.github.com>
2018-10-30 09:30:23 +0800

    36666-2
    36666-2

--- main.lua
+++ main.lua
@@ -13,3 +13,6 @@ msgText:setFillColor( 0.2, 0.6, 0.8 )
 
 local msgText = display.newText( "Hi, world!", world.x, world.y+190, native.systemFont, 32 )
 msgText:setFillColor( 0.2, 0.6, 0.8 )
+
+local msgText = display.newText( "Hi, world!", world.x, world.y+220, native.systemFont, 32 )
+msgText:setFillColor( 0.2, 0.6, 0.8 )
